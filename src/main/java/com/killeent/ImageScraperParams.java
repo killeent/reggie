b9a2b462a41600ff7b64bd06b3fba67be5df15ee
Trevor Killeen <killeent@cs.washington.edu>
2014-10-19 18:01:14 -0700

    setup basic API; command line argument parsing; organize project for Maven; add dependency to Apache CLI tools

---
+++ src/main/java/com/killeent/ImageScraperParams.java
@@ -0,0 +1,48 @@
+package com.killeent;
+
+/**
+ * Original Author: Trevor Killeen (2014)
+ *
+ * TODO: refactor this into a builder pattern?
+ *
+ * Parameters to pass to an Image Scraper which specify the behavior
+ * of a call to {@link com.killeent.ImageScraper#ScrapePage(java.net.URI, String,
+ * ImageScraperParams)}
+ */
+public class ImageScraperParams {
+
+    private int maxDepth;
+    private boolean followOutboundLinks;
+
+    private int maxDepth() {
+        return maxDepth;
+    }
+
+    private boolean followOutboundLinks() {
+        return followOutboundLinks;
+    }
+
+    /**
+     * Constructs ImageScraperParams that define behavior when scraping pages
+     * for images.
+     *
+     * @param maxDepth The maximum depth of web pages we should explore when
+     *                 crawling the web. If this value is 0, we will only
+     *                 scrape images from the initial page specified by the URL.
+     *                 If it is 1, we will crawl all pages linked from the specified
+     *                 URL and scrape images from those pages as well. If it is 2,
+     *                 we will crawl all pages linked from pages linked from URL. And
+     *                 so forth.
+     * @param followOutboundLinks If true, we will crawl links to pages outside of the
+     *                            base URL. For example, if our url is specified by
+     *                            "https://abc.com/pics", and this is false, we will
+     *                            only crawl pages with "abc.com" as their base path.
+     *                            If true, we could jump to another site, e.g. if
+     *                            /pics links to "https://xyz.com/images"
+     */
+    public ImageScraperParams(int maxDepth, boolean followOutboundLinks) {
+        this.maxDepth = maxDepth;
+        this.followOutboundLinks = followOutboundLinks;
+    }
+
+}
---
+++ src/main/java/com/killeent/Reggie.java
@@ -0,0 +1,96 @@
+package com.killeent;
+
+import org.apache.commons.cli.*;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Specifies the CLI to Reggie.
+ */
+public class Reggie {
+
+    private static final int DEFAULT_MAX_DEPTH = 3;
+    private static final boolean DEFAULT_FOLLOW_OUTBOUND_LINKS = false;
+
+    private static final String DEPTH_FLAG = "depth";
+    private static final String OUTBOUND_FLAG = "outbound";
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            usage();
+            System.exit(1);
+        }
+
+        // Define command line flags
+        Options options = new Options();
+        options.addOption(OptionBuilder.withLongOpt(DEPTH_FLAG)
+                                       .withDescription("crawl to a maximum depth of n")
+                                       .hasArg()
+                                       .withArgName("n")
+                                       .create());
+        options.addOption(OUTBOUND_FLAG, false, "crawl outbound links");
+
+        // Necessary arguments
+        boolean followOutboundLinks = DEFAULT_FOLLOW_OUTBOUND_LINKS;
+        int maxDepth = DEFAULT_MAX_DEPTH;
+        URI uri = null;
+        String directory = null;
+
+        // Tries to extract the data for web scraping from the command line arguments
+        CommandLineParser parser = new BasicParser();
+        CommandLine commandLine;
+        try {
+            // will throw exception if cannot parse
+            commandLine = parser.parse(options, args);
+
+            // parse flags
+            followOutboundLinks = commandLine.hasOption(OUTBOUND_FLAG);
+            maxDepth = Integer.parseInt(
+                    commandLine.getOptionValue(DEPTH_FLAG, String.valueOf(DEFAULT_MAX_DEPTH)));
+
+            // parse URI and directory
+            String[] leftovers = commandLine.getArgs();
+            if (leftovers.length != 2) {
+                // we are missing something
+                usage();
+                System.exit(1);
+            }
+
+            // will throw exception if not valid URI
+            uri = new URI(leftovers[0]);
+
+            // check if valid directory
+            directory = leftovers[1];
+            File file = new File(directory);
+            if (!file.isDirectory()) {
+                System.out.println("Invalid Directory");
+                System.exit(1);
+            }
+        } catch (ParseException p) {
+            usage();
+            System.exit(1);
+        } catch (URISyntaxException e) {
+            System.out.println("Invalid URI");
+            System.exit(1);
+        }
+
+        // now construct the parameters
+        ImageScraperParams params = new ImageScraperParams(maxDepth, followOutboundLinks);
+
+        // okay we have everything, let's scrape some images! (placeholder for now)
+        ImageScraper scraper = new ImageScraper() {
+            @Override
+            public void ScrapePage(URI url, String path, ImageScraperParams params) {
+
+            }
+        };
+
+        scraper.ScrapePage(uri, directory, params);
+    }
+
+    private static void usage() {
+        System.out.println("Usage: java Reggie [-depth=n | -outbound ] uri output_directory");
+    }
+}
